449f2afdce94b01b35003e0e7d492292ff1a80b5
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-19 17:53:32 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  days=[]
-  holiday_hash.collect {|ssn,day| day.collect {|day,supply| days << day if supply.include?("BBQ")}}
-  return days
+  holiday_hash.collect {|ssn,day| day.collect {|day,supply| days << day if supply.include?("BBQ")}}.flatten.compact
 end
